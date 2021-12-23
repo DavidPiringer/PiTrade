6e6ba9addd69544183de9efd799df9a9bffe29c7
David Piringer <david.piringer.1996@gmail.com>
2021-12-23 23:36:36 +0100

    add a abstract base strategy

---
+++ src/PiTrade.Strategy/Stategy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PiTrade.Exchange;
+using PiTrade.Exchange.Entities;
+
+namespace PiTrade.Strategy {
+  public abstract class Stategy : IOrderListener {
+    protected IMarket Market { get; }
+    protected IMarketHandle? Handle { get; private set; }
+
+    protected Stategy(IMarket market) {
+      this.Market = market;
+    }
+
+
+    public async Task Run() {
+      Handle = Market.GetMarketHandle(out Task awaitTask, this);
+      await awaitTask;
+    }
+
+    public abstract Task OnBuy(Order order);
+    public abstract Task OnPriceUpdate(decimal price);
+    public abstract Task OnSell(Order order);
+  }
+}
