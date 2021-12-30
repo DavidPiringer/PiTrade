19fc0cdcde919086bcf82986faf5030a15685208
David Piringer <david.piringer.1996@gmail.com>
2021-12-30 17:10:42 +0100

    add a utility class for exponential backoff

---
+++ src/PiTrade.Networking/ExponentialBackoff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiTrade.Networking {
+  public static class ExponentialBackoff {
+    public static async Task Try(Func<Task<bool>> comparison, int startDelay = 500) {
+      int delay = startDelay;
+      while (await comparison()) {
+        await Task.Delay(delay);
+        delay *= 2;
+      }
+    }
+  }
+}
