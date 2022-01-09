a8f0c8d5dcb68cd35403c0a5d01869be2eed7b8e
David Piringer <david.piringer.1996@gmail.com>
2022-01-09 17:12:35 +0100

    add SMA

---
+++ src/PiTrade.Exchange/Indicators/SimpleMovingAverage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PiTrade.Exchange.Enums;
+
+namespace PiTrade.Exchange.Indicators {
+  public class SimpleMovingAverage : Indicator {
+
+    public SimpleMovingAverage(TimeSpan period, int maxTicks = 100, IndicatorValueType indicatorValueType = IndicatorValueType.Close)
+      : base(period, maxTicks, indicatorValueType) { }
+
+    protected override decimal Calculate(decimal value) =>
+      (Value * (maxTicks - 1) + value) / maxTicks; 
+
+  }
+}
