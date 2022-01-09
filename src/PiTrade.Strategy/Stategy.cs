2d159fa37441c8f54ef58542edb8692715b0d21d
David Piringer <david.piringer.1996@gmail.com>
2022-01-09 17:12:50 +0100

    refactor strategy base and WaveSurferStrategy

--- src/PiTrade.Strategy/Stategy.cs
+++ src/PiTrade.Strategy/Stategy.cs
@@ -5,17 +5,54 @@ using System.Text;
 using System.Threading.Tasks;
 using PiTrade.Exchange;
 using PiTrade.Exchange.Entities;
+using PiTrade.Exchange.Enums;
+using PiTrade.Exchange.Extensions;
+using PiTrade.Logging;
 
 namespace PiTrade.Strategy {
   public abstract class Stategy {
+    protected const decimal CommissionFee = 0.00075m;
+
     protected IMarket Market { get; }
+    protected decimal Quantity { get; private set; }
+    protected static decimal Commission { get; private set; }
+    protected static decimal Revenue { get; private set; }
+
+
+    private static readonly object locker = new object();
+    private static decimal Profit { get; set; }
+
 
     protected Stategy(IMarket market) {
       this.Market = market;
     }
 
-
     public virtual async Task Run() => await Market.Connect();
 
+
+    protected void AddFilledOrder(Order order) {
+      if(order.IsFilled) {
+        lock(locker) {
+          Commission += order.Amount * CommissionFee;
+          if (order.Side == OrderSide.BUY) {
+            Revenue -= order.Amount;
+            Quantity += order.Quantity;
+          } else if (order.Side == OrderSide.SELL) {
+            Revenue += order.Amount;
+            Quantity -= order.Quantity;
+            Profit += Revenue - Commission;
+            Log.Info($"Profit = {Profit}");
+          }
+          Quantity = Quantity.RoundDown(Market.AssetPrecision);
+        }
+      }
+    }
+
+    protected virtual void Reset() {
+      lock(locker) {
+        Quantity = 0;
+        Revenue = 0;
+      }
+    }
   }
 }
--- src/PiTrade.Strategy/WaveSurferStrategy.cs
+++ src/PiTrade.Strategy/WaveSurferStrategy.cs
@@ -14,26 +14,21 @@ using PiTrade.Strategy.Util;
 
 namespace PiTrade.Strategy {
   public class WaveSurferStrategy : Stategy {
-    private const decimal CommissionFee = 0.00075m;
     private const decimal UpperThreshold = 0.002m;
     private const decimal LowerThreshold = 0.01m;
 
     private readonly IEnumerable<IIndicator> indicators;
     private readonly decimal allowedQuote;
-    private readonly object locker = new object();
 
     private Order? buyOrder;
     private Order? sellOrder;
 
-    private decimal Quantity { get; set; } = 0m;
-    private decimal Commission { get; set; } = 0m;
-    private decimal Revenue { get; set; } = 0m;
     private bool AllIndicatorsReady => indicators.All(x => x.IsReady);
     private bool AllIndicatorsPositive => indicators.All(x => x.Slope > 0);
     private bool AnyIndicatorsNegative => indicators.Any(x => x.Slope < 0);
 
 
-    private Func<Task>? State { get; set; }
+    private Func<decimal, Task>? State { get; set; }
 
 
     public WaveSurferStrategy(IMarket market, decimal allowedQuote, bool respendMoney) : base(market) {
@@ -57,13 +52,13 @@ namespace PiTrade.Strategy {
 
     // TODO: add PiTrade.EventHandling -> convinient stuff for events
 
-    private async Task MarketListener(decimal price) {
+    private async Task MarketListener(decimal currentPrice) {
       if (State != null && AllIndicatorsReady) 
-        await State();
+        await State(currentPrice);
       else Log.Info("STATE = null");
     }
 
-    private async Task PrepareBuy() {
+    private async Task PrepareBuy(decimal currentPrice) {
       Log.Info("STATE = PrepareBuy");
       if (buyOrder != null) return;
 
@@ -82,140 +77,43 @@ namespace PiTrade.Strategy {
       }
     }
 
-    private async Task PrepareSell() {
+    private async Task PrepareSell(decimal currentPrice) {
       Log.Info("STATE = PrepareSell");
       if (sellOrder != null) return;
-      //TODO: add indicator listen to market -> market gives indicator array and current price
-      if (buyOrder != null && AnyIndicatorsNegative) {
-        var price = Market.CurrentPrice;
-        if (price > buyOrder.AvgFillPrice * (1m + UpperThreshold) ||
-            price < buyOrder.AvgFillPrice * (1m - LowerThreshold)) {
-          State = null;
-          Log.Info("SELL");
-          (Order? order, ErrorState error) = await Market.CreateMarketOrder(OrderSide.SELL, buyOrder.Quantity);
-          if (error == ErrorState.None && order != null) {
-            this.sellOrder = order;
-            order.WhenFilled(SellFinished);
-          } else {
-            Log.Error($"Error for SELL -> {error}");
-          }
+
+      if (buyOrder != null && AnyIndicatorsNegative &&
+         (currentPrice > buyOrder.AvgFillPrice * (1m + UpperThreshold) ||
+          currentPrice < buyOrder.AvgFillPrice * (1m - LowerThreshold))) {
+        State = null;
+        Log.Info("SELL");
+
+        (Order? order, ErrorState error) = await Market.CreateMarketOrder(OrderSide.SELL, buyOrder.Quantity);
+        if (error == ErrorState.None && order != null) {
+          this.sellOrder = order;
+          order.WhenFilled(SellFinished);
+        } else {
+          Log.Error($"Error for SELL -> {error}");
         }
       }
     }
 
     private void BuyFinished(Order buyOrder) {
       Log.Info("STATE = BuyFinished");
-      lock (locker) {
-        Quantity += buyOrder.Quantity;
-        Quantity = Quantity.RoundDown(Market.AssetPrecision);
-        Commission += buyOrder.Amount * CommissionFee;
-        Revenue -= buyOrder.Amount;
-        State = PrepareSell;
-      }
+      AddFilledOrder(buyOrder);
+      State = PrepareSell;
     }
 
     private void SellFinished(Order sellOrder) {
       Log.Info("STATE = SellFinished");
-      lock (locker) {
-        Quantity -= sellOrder.Quantity;
-        Quantity = Quantity.RoundDown(Market.AssetPrecision);
-        Commission += sellOrder.Amount * CommissionFee;
-        Revenue += sellOrder.Amount;
-        this.buyOrder = null;
-        this.sellOrder = null;
-        ProfitCalculator.Add(Revenue - Commission);
-        ProfitCalculator.PrintStats();
-        Revenue = 0m;
-        Commission = 0m;
-        State = PrepareBuy;
-      }
-    }
-    /*
-    public override async Task OnBuy(Order order) {
-      var lastFill = order.Fills.LastOrDefault();
-      if(lastFill != null) {
-        lock(locker) {
-          Quantity += lastFill.Quantity;
-          Quantity = Quantity.RoundDown(Market.AssetPrecision);
-          Commission += lastFill.Amount * CommissionFee;
-          Revenue -= lastFill.Amount;
-        }
-      }
-      
-      await Task.CompletedTask;
+      AddFilledOrder(sellOrder);
+      Reset();
+      State = PrepareBuy;
     }
 
-    public override async Task OnPriceUpdate(decimal price) {
-      if (Handle == null) return;
-
-      if(ema5T36.IsReady && lastEma15T8 != ema5T36.Value && ema5T120.IsReady) {
-        var minSellPrice = buyOrder != null && buyOrder.IsFilled ? buyOrder.AvgFillPrice * (1m + UpperThreshold) : 0m;
-        var emergencySellPrice = buyOrder != null && buyOrder.IsFilled ? buyOrder.AvgFillPrice * (1m - LowerThreshold) : 0m;
-        Log.Info(
-          $"price = {price.ToString("F2", CultureInfo.InvariantCulture)}, " +
-          $"ema5T36 = {ema5T36.Slope.ToString("F2", CultureInfo.InvariantCulture)}, " +
-          $"ema5T120 = {ema5T120.Slope.ToString("F2", CultureInfo.InvariantCulture)}, " +
-          $"minSellPrice = {minSellPrice.ToString("F2", CultureInfo.InvariantCulture)}, " +
-          $"emergencySellPrice = {emergencySellPrice.ToString("F2", CultureInfo.InvariantCulture)}");
-        lastEma15T8 = ema5T36.Value;
-
-        if(buyOrder == null && ema5T36.Slope > 0 && ema5T120.Slope > 0) {
-          buyOrder = await Handle.BuyLimit(price, allowedQuote / price);
-          Log.Info("BUY");
-        } else if (!IsSelling && 
-          buyOrder != null && buyOrder.IsFilled && 
-          sellOrder == null &&
-          price > buyOrder.AvgFillPrice * (1m + UpperThreshold) &&
-          ema5T36.Slope < 0) {
-          IsSelling = true; // is needed because of the async structure
-          if (sellOrder2 != null) await Handle.Cancel(sellOrder2);
-          sellOrder = await Handle.Market(OrderSide.SELL, buyOrder.Quantity);
-          Log.Info("SELL");
-        } else if(!IsSelling && buyOrder != null && buyOrder.IsFilled) {
-          if (sellOrder2 != null) await Handle.Cancel(sellOrder2);
-          sellOrder2 = await Handle.SellLimit(price * (1m + UpperThreshold * 1.5m), buyOrder.Quantity);
-        }
-      }
-      
-      if(!IsSelling && 
-        buyOrder != null && buyOrder.IsFilled && 
-        sellOrder == null &&
-        price < buyOrder.AvgFillPrice * (1m - LowerThreshold)) {
-        IsSelling = true; // is needed because of the async structure
-        if (sellOrder2 != null) await Handle.Cancel(sellOrder2);
-        sellOrder = await Handle.Market(OrderSide.SELL, buyOrder.Quantity); // TODO: Order = Task ??? create own TPL with orders? 
-        Log.Warn($"Emergency Sell, Price Dropped too fast (cur. Price = {price}, Buy Price = {buyOrder.AvgFillPrice})");
-      }
+    protected override void Reset() {
+      base.Reset();
+      this.buyOrder = null;
+      this.sellOrder = null;
     }
-
-    public override async Task OnSell(Order order) {
-      var lastFill = order.Fills.LastOrDefault();
-      if (lastFill != null) {
-        lock (locker) {
-          Quantity -= lastFill.Quantity;
-          Quantity = Quantity.RoundDown(Market.AssetPrecision); 
-          Commission += lastFill.Amount * CommissionFee;
-          Revenue += lastFill.Amount;
-        }
-      }
-      if (order.IsFilled) {
-        Log.Info($"CLEAR {Market.Asset}{Market.Quote}");
-        try {
-          await CommissionManager.Add(Commission);
-        } catch (Exception ex) {
-          Log.Error(ex);
-        }
-        lock (locker) {
-          buyOrder = null;
-          sellOrder = null;
-          sellOrder2 = null;
-          IsSelling = false;
-          ProfitCalculator.Add(Revenue - Commission);
-          ProfitCalculator.PrintStats();
-          Revenue = 0m;
-          Commission = 0m;
-        }
-      }
-    }*/
   }
 }
