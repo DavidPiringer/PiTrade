039e13a0c6b0fff079b6c44030bc21cd8066c69a
David Piringer <david.piringer.1996@gmail.com>
2022-01-19 19:24:52 +0100

    fix naming

--- src/PiTrade.Strategy/Strategy.cs
+++ src/PiTrade.Strategy/Strategy.cs
@@ -10,23 +10,23 @@ using PiTrade.Exchange.Extensions;
 using PiTrade.Logging;
 
 namespace PiTrade.Strategy {
-  public abstract class Stategy {
+  public abstract class Strategy {
 
     public static IMarket? CommissionMarket { get; set; }
 
     protected const decimal CommissionFee = 0.00075m;
 
     protected IMarket Market { get; }
-    protected static decimal Expenses { get; private set; }
-    protected static decimal Returns { get; private set; }
-    protected static decimal Commission { get; private set; }
+    protected decimal Expenses { get; private set; }
+    protected decimal Returns { get; private set; }
+    protected static decimal OpenCommission { get; private set; }
 
 
     private static readonly object locker = new object();
     private static decimal Profit { get; set; }
 
 
-    protected Stategy(IMarket market) {
+    protected Strategy(IMarket market) {
       this.Market = market;
     }
 
@@ -35,23 +35,26 @@ namespace PiTrade.Strategy {
 
     protected void AddFilledOrder(Order order) {
       decimal tmpCommission = 0;
-      if(order.IsFilled) {
+      decimal commissionOfOrder = order.Amount * CommissionFee;
+      if (order.IsFilled) {
         lock(locker) {
-          Commission += order.Amount * CommissionFee;
-          tmpCommission = Commission;
+          OpenCommission += commissionOfOrder;
+          tmpCommission = OpenCommission;
           if(tmpCommission > 15) {
             Expenses += tmpCommission;
-            Commission = 0;
+            OpenCommission = 0;
           }
-          if (order.Side == OrderSide.BUY)
+          if (order.Side == OrderSide.BUY) {
             Expenses += order.Amount;
-          else if (order.Side == OrderSide.SELL)
+            Expenses += commissionOfOrder;
+          } else if (order.Side == OrderSide.SELL) {
             Returns += order.Amount;
-          Profit = Returns - Expenses - Commission;
+            Expenses += commissionOfOrder;
+            Profit += Returns - Expenses;
+          }
 
-          if (Profit < -10) {
-            Log.Error("Closing Application because of great losses.");
-            Environment.Exit(-1);
+          if (Profit < -20m) {
+            EmergencyStop();
           }
         }
       }
@@ -62,11 +65,17 @@ namespace PiTrade.Strategy {
       }
     }
 
-    protected virtual void Reset() { }
+    protected virtual void EmergencyStop() {
+      Log.Error("Closing Application because of great losses.");
+    }
+
+    protected virtual void Reset() {
+      PrintStatus();
+    }
 
     protected virtual void PrintStatus() {
+      Log.Info($"Reset {Market.Asset}/{Market.Quote}");
       Log.Info($"Profit = {Profit}");
-      Log.Info($"Commission = {Commission}");
     }
   }
 }
