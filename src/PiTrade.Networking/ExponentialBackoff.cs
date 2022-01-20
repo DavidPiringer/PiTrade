88d47306f49f6837f823232f6effad7e463e4758
David Piringer <david.piringer.1996@gmail.com>
2022-01-20 21:40:05 +0100

    add retries

--- src/PiTrade.Networking/ExponentialBackoff.cs
+++ src/PiTrade.Networking/ExponentialBackoff.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 
 namespace PiTrade.Networking {
   public static class ExponentialBackoff {
-    public static async Task Try(Func<Task<bool>> comparison, int startDelay = 500) {
+    public static async Task Try(Func<Task<bool>> comparison, int startDelay = 500, int tries = 10) {
       int delay = startDelay;
-      while (await comparison()) {
+      int count = 0;
+      while (await comparison() && count < tries) {
         await Task.Delay(delay);
         delay *= 2;
+        count++;
       }
     }
   }
