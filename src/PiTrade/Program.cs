cc2d160bb5b874207c2934b57d0ea44ae0a15554
David Piringer <david.piringer.1996@gmail.com>
2022-01-30 15:14:29 +0100

    fix main program

--- src/PiTrade/Program.cs
+++ src/PiTrade/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Newtonsoft.Json.Linq;
 using PiTrade.Exchange;
+using PiTrade.Exchange.Base;
 using PiTrade.Exchange.Binance;
 using PiTrade.Exchange.Entities;
 using PiTrade.Exchange.Indicators;
@@ -17,20 +18,9 @@ var secret = config["secret"]?.ToString();
 if (key == null || secret == null)
   throw new Exception("Key or Secret is null.");
 
-/*
- * TODO:
- * - Add IOrder interface .. done
- * - merge BinanceMarket & BinanceExchange -> BinanceAPI
- * - move BaseClasses into separate folder
- * - split Exchange BaseClass -> BasicExchange / Exchange
- * - Add GenericParameter for Exchange BaseClasses
- *   - BasicExchange -> T : IExchangeAPI
- *   - Exchange -> T: IExchangeAPI, IExchangeStream / Listener
- * - Remove Connect/Disconnect from Market ???
- * 
- */
-var exchange = new BinanceExchange(key, secret);
-var market = exchange.GetMarket(Symbol.ETH, Symbol.USDT);
+var client = new BinanceStreamAPIClient(key, secret);
+var exchange = new Exchange(client);
+var market = await exchange.GetMarket(Symbol.ETH, Symbol.USDT);
 if(market != null) {
   var strategy = new GridTradingStrategy(market, 30.0m, 2640m, 2620m, 10, 0.005m); // TODO: better grid calc between bounds (nicht die ränder verwenden) -> x% * (max-min)
   // TODO: gegenchecken der orders (!isFilled || !isCancelled) in zeitabständen (zwecks der sicherheit)
@@ -38,7 +28,5 @@ if(market != null) {
   strategy.Enable();
 }
 
-
-
-await exchange.Run(CancellationToken.None);
+exchange.Run(CancellationToken.None);
 
