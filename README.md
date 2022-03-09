b754c293a751a8c19fc4e32262eaa20d0fab14a3
David Piringer <david.piringer.1996@gmail.com>
2022-03-09 23:10:49 +0100

    added mqtt test suite

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PiTrade
+
+https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets?view=aspnetcore-6.0&tabs=linux
---
+++ services/MQTTBroker/MQTTBroker/Program.cs
@@ -0,0 +1,22 @@
+﻿// Start a MQTT server.
+using MQTTnet;
+using MQTTnet.Server;
+using System.Text;
+// Configure MQTT server.
+var optionsBuilder = new MqttServerOptionsBuilder();
+var mqttServer = new MqttFactory().CreateMqttServer();
+await mqttServer.StartAsync(optionsBuilder.Build());
+mqttServer.UseApplicationMessageReceivedHandler(e =>
+{
+  Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
+  Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+  Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+  Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+  Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+  Console.WriteLine();
+
+});
+Console.WriteLine(mqttServer.IsStarted);
+Console.WriteLine("Press any key to exit.");
+Console.ReadLine();
+await mqttServer.StopAsync();
