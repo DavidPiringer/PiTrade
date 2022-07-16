72e96acaf95e0338aaef3be31869b5418f3907e0
David Piringer <david.piringer.1996@gmail.com>
2022-07-16 22:50:05 +0200

    Adapt main program

--- src/PiTrade/Program.cs
+++ src/PiTrade/Program.cs
@@ -10,7 +10,11 @@ using PiTrade.Exchange.Indicators;
 using PiTrade.Logging;
 using PiTrade.Strategy;
 
+#if DEBUG
 var configPath = args.FirstOrDefault(@"C:\Users\David\Documents\binanceTestConfig.json");
+#else
+var configPath = args.FirstOrDefault(@"C:\Users\David\Documents\binanceConfig.json");
+#endif
 var config = JObject.Parse(File.ReadAllText(configPath));
 
 var key = config["key"]?.ToString();
@@ -23,30 +27,8 @@ if (key == null || secret == null)
 var exchange = await BinanceExchange.Create(key, secret);
 var markets = exchange.Markets;
 var btcbusd = markets.Where(x => x.BaseAsset == Symbol.BTC && x.QuoteAsset == Symbol.BUSD).First();
-
-var strategy = new StandardDeviationStrategy(btcbusd, 15m, TimeSpan.FromSeconds(6), 3);
+var strategy = new StandardDeviationStrategy(btcbusd, 20m, 0.5m, TimeSpan.FromSeconds(5), 18);
 strategy.Start();
-/*
-var order1 = await btcbusd.Sell(0.01m)
-  .OnTrade((o, t) => {
-    global::System.Console.WriteLine(o);
-  })
-  .OnExecuted(o => {
-    global::System.Console.WriteLine($"Executed {o.Id}");
-  })
-  .Submit();
-
-var order2 = await btcbusd
-  .Buy(0.01m)
-  .For(25000m)
-  .OnTrade((o, t) => {
-    global::System.Console.WriteLine(o);
-  })
-  .OnExecutedAsync(o => Task.Run(() => {
-    global::System.Console.WriteLine($"Executed {o.Id}");
-  }))
-  .Submit();
-*/
 
 Console.ReadLine();
 
